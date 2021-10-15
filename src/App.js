3cf2cbb9f7e61b3dacaa9b65c554ecba7f0383ba
Rory <rory.lindsay@pulselive.com>
2021-10-15 19:13:30 +0100

    feat: state for dropdown

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Accordion from './components/Accordion';
 // import Search from './components/Search';
 import Dropdown from './components/Dropdown';
@@ -24,19 +24,24 @@ const options = [
         value: 'red'
     },
     {
-        label: 'A Color Green',
+        label: 'A Bright Green',
         value: 'green'
     },
     {
-        label: 'A shade of Blue',
+        label: 'A Shade of Blue',
         value: 'blue'
     },
 ];
 
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
     <div>
-        <Dropdown options={options} />
+        <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options} />
     </div>
     );
 };
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Dropdown = ({ options }) => {
+const Dropdown = ({ options, selected, onSelectedChange }) => {
     const renderedOptions = options.map((option) => {
+
+        if ( option.value === selected.value ) {
+            return null;
+        }
+
         return (
-            <div key={option.value} className='item'>
+            <div 
+            key={option.value} 
+            className='item'
+            onClick={() => onSelectedChange(option)}
+            >
                 {option.label}
             </div>
         );
@@ -15,7 +24,7 @@ const Dropdown = ({ options }) => {
                 <label className="label">Select a Colour</label>
                 <div className="ui selection dropdown visible active">
                     <i className="dropdown icon"></i>
-                    <div className="text">Select Colour</div>
+                    <div className="text">{selected.label}</div>
                     <div className="menu visible transition">
                         {renderedOptions}
                     </div>
